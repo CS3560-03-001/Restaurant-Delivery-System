60dd6a47c8411b86daff89d5f1e5bcce022d66de
Anton C. <anton.2.clark@gmail.com>
2026-03-20 16:44:35 -0700

    Added skeletons for some Suppplements.

--- Supplements/DeliveryAssignment.java
+++ Supplements/DeliveryAssignment.java
@@ -1,3 +1,25 @@
-public class DeliveryAssignment {
+import java.time.LocalDateTime;
 
+public class DeliveryAssignment {
+    // Vars
+    private final String assignmentID;
+    private final LocalDateTime assignedAt;
+    public LocalDateTime estimatedArrival;
+    public LocalDateTime deliveredAt;
+    public String currentLocation;
+
+    // Accessors
+    public String GetAssignmentID() {
+        return assignmentID;
+    }
+
+    public LocalDateTime GetAssignedAt() {
+        return assignedAt;
+    }
+
+    // Methods
+    public DeliveryAssignment(String assignmentID, LocalDateTime assignedAt) {
+        this.assignmentID = assignmentID;
+        this.assignedAt = assignedAt;
+    }
 }
